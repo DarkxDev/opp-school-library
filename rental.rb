cdcc266519cad62bd2e27d764537c9543500cda2
DarkxDev <bryanfernandohurtado@gmail.com>
2023-04-12 23:32:53 -0500

    Added aditional class methods

--- rental.rb
+++ rental.rb
@@ -9,18 +9,3 @@ class Rental
     person.rentals << self
   end
 end
-
-#
-#   Test this code by running the following:
-#
-#   person1 = Person.new(10, "John")
-#   book1 = Book.new("The Hitchhiker's Guide to the Galaxy", "Douglas Adams")
-#   rental1 = Rental.new(Date.today, book1, person1)
-#
-#   def has_rented_book?(person, book)
-#     person.rentals.any? { |rental| rental.book == book }
-#   end
-#
-#   puts has_rented_book?(person1, book1) # true
-#   puts has_rented_book?(person1, book2) # false
-#
