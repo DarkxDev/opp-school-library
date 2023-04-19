7ed7c9c6a68dbf70a9f63f8e02124ebce692603a
DarkxDev <bryanfernandohurtado@gmail.com>
2023-04-19 13:50:52 -0500

    Fix linters

--- main.rb
+++ main.rb
@@ -1,5 +1,3 @@
-# rubocop:disable Metrics/CyclomaticComplexity
-
 require_relative './app'
 require_relative './menu'
 
@@ -9,33 +7,17 @@ def main
   app.load_people('people.json')
   app.load_rentals('rentals.json')
 
-  menu = Menu.new
+  menu = Menu.new(app)
 
   puts '
   Welcome to School Library App!'
 
   loop do
     menu.display_options
+
     choice = gets.chomp.to_i
-
-    case choice
-    when 1 then app.list_books
-    when 2 then app.list_people
-    when 3 then app.create_person
-    when 4 then app.create_book
-    when 5 then app.create_rental
-    when 6 then app.list_rentals
-    when 7
-      app.save_books('books.json')
-      app.save_people('people.json')
-      app.save_rentals('rentals.json')
-
-      puts 'Thanks for using this app!'
-      exit
-    end
+    menu.perform_action(choice)
   end
 end
 
 main
-
-# rubocop:enable Metrics/CyclomaticComplexity
--- menu.rb
+++ menu.rb
@@ -1,4 +1,10 @@
+# rubocop:disable Metrics/CyclomaticComplexity
+
 class Menu
+  def initialize(app)
+    @app = app
+  end
+
   def display_options
     puts '
       Please choose an option by entering a number
@@ -11,4 +17,26 @@ class Menu
         6 - List all rentals for a given person id
         7 - Exit'
   end
+
+  def perform_action(choice)
+    case choice
+    when 1 then @app.list_books
+    when 2 then @app.list_people
+    when 3 then @app.create_person
+    when 4 then @app.create_book
+    when 5 then @app.create_rental
+    when 6 then @app.list_rentals
+    when 7
+      @app.save_books('books.json')
+      @app.save_people('people.json')
+      @app.save_rentals('rentals.json')
+
+      puts 'Thanks for using this app!'
+      exit
+    else
+      puts 'Invalid option. Please choose again.'
+    end
+  end
 end
+
+# rubocop:enable Metrics/CyclomaticComplexity
